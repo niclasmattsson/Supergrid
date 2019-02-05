51c254fea6f5b9e784c13748aee878c1a4135a9c
niclasmattsson <33093224+niclasmattsson@users.noreply.github.com>
2019-02-05 11:53:25 +0100

    Flip legend in stacked bar plots

--- src/helperfunctions.jl
+++ src/helperfunctions.jl
@@ -4,6 +4,24 @@ flip(x) = permutedims(x, (2,1))
 
 showall(x) = show(stdout, "text/plain", x)
 
+# groupedbar(rand(10,5), bar_position = :stack, lab=["A" "B" "C" "D" "E"])
+# groupedbar([1:10;12;12], [rand(10,5) zeros(10,5); zeros(2,10)], bar_position = :stack,
+#         lab=["" "" "" "" "" "A" "B" "C" "D" "E"], color=[1 2 3 4 5 5 4 3 2 1], xlim=(0,11))
+
+function groupedbarflip(data; args...)
+    newargs = Dict(args)
+	nbars, ncolors = size(data)
+	x = [1:nbars; nbars+2; nbars+2]
+	newdata = [data zeros(nbars,ncolors); zeros(2,ncolors*2)]
+    if haskey(newargs, :label)
+	   newargs[:label] = [permutedims(repeat([""], ncolors)) reverse(newargs[:label],dims=2)]
+    end
+    colors = get(newargs, :color, (1:ncolors)')
+    newargs[:color] = [colors reverse(colors,dims=2)]
+    newargs[:xlim] = (0, nbars+1)
+    groupedbar(x, newdata; newargs...)
+end
+
 @userplot StackedArea
 
 # a simple "recipe" for Plots.jl to get stacked area plots
@@ -22,7 +40,21 @@ showall(x) = show(stdout, "text/plain", x)
     end
 end
 
-
+@userplot Areaplot
+@recipe function f(a::Areaplot)
+    data = cumsum(a.args[1], dims=2)
+    seriestype := :line
+    fillrange := 0
+    @series begin
+        data[:,1]
+    end
+    for i in 2:size(data, 2)
+    @series begin
+            fillrange := data[:,i-1]
+            data[:,i]
+        end
+    end
+end
 
 #=
 function printtable(title::String, setnames::Vector, datarows::Array, columnname::Symbol = :none)
