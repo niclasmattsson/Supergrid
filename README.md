14f2fbdb1687d0c7d5179ec2ae8f572f22da25af
niclasmattsson <33093224+niclasmattsson@users.noreply.github.com>
2019-02-12 15:20:32 +0100

    Updated README

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A capacity expansion model of the electricity system for arbitrary world regions
 Type `]` to enter package mode, then:
 
 ```
-(v1.1) pkg> add https://github.com/niclasmattsson/Supergrid.jl
+(v1.1) pkg> add https://github.com/niclasmattsson/Supergrid
 ``` 
 
 ## Running the model
@@ -29,8 +29,9 @@ If you forget the last semicolon a **LOT** of results will get dumped to the con
 
 ```
 defaultoptions() = Dict(
+        :regionset => :europe8,             # :eurasia21, :europe8
         :carbontax => 0.0,                  # €/ton CO2
-        :carboncap => 1.0,                  # global cap in kg CO2/kWh elec (BAU scenario: ~0.5 kgCO2/kWh elec)
+        :carboncap => 1.0,                  # global cap in kg CO2/kWh elec  (BAU scenario: ~0.5 kgCO2/kWh elec)
         :maxbioenergy => 0.05,              # max share of biofuel of annual regional electricity demand (assuming CCGT, less if GT) 
         :nuclearallowed => true,
         :globalnuclearlimit => Inf,         # maximum total nuclear capacity in all regions (GW)
@@ -46,7 +47,9 @@ defaultoptions() = Dict(
         :rampingconstraints => false,
         :rampingcosts => false,
         :disabletechs => [],
-        :resultsfile => "results.jld2"		# use "" to skip saving the results 
+        :disableregions => [],
+        :islandindexes => [],               # [1:8, 9:15, 16:21] for eurasia21
+        :resultsfile => "results.jld2"      # use "" to skip saving the results in the database
     )
 ```
 
--- src/Supergrid.jl
+++ src/Supergrid.jl
@@ -13,102 +13,102 @@ include("output.jl")
 include("iewruns.jl")
 
 defaultoptions() = Dict(
-		:regionset => :europe8,				# :eurasia21, :europe8
-		:carbontax => 0.0,					# €/ton CO2
-		:carboncap => 1.0,					# global cap in kg CO2/kWh elec  (BAU scenario: ~0.5 kgCO2/kWh elec)
-		:maxbioenergy => 0.05,				# max share of biofuel of annual regional electricity demand (assuming CCGT, less if GT) 
-		:nuclearallowed => true,
-		:globalnuclearlimit => Inf,			# maximum total nuclear capacity in all regions (GW)
-		:hydroinvestmentsallowed => false,
-		:transmissionallowed => :all,		# :none, :islands, :all
-		:hours => 1,						# 1,2,3 or 6 hours per period
-		:solarwindarea => 1,				# area multiplier for GIS solar & wind potentials
-		:selectdays => 1,
-		:skipdays => 0,
-		:solver => :cplex,
-		:threads => 3,
-		:showsolverlog => true,
-		:rampingconstraints => false,
-		:rampingcosts => false,
-		:disabletechs => [],
-		:disableregions => [],
-		:islandindexes => [],				# [1:8, 9:15, 16:21] for eurasia21
-		:resultsfile => "results.jld2"		# use "" to skip saving the results in the database
-	)
+        :regionset => :europe8,             # :eurasia21, :europe8
+        :carbontax => 0.0,                  # €/ton CO2
+        :carboncap => 1.0,                  # global cap in kg CO2/kWh elec  (BAU scenario: ~0.5 kgCO2/kWh elec)
+        :maxbioenergy => 0.05,              # max share of biofuel of annual regional electricity demand (assuming CCGT, less if GT) 
+        :nuclearallowed => true,
+        :globalnuclearlimit => Inf,         # maximum total nuclear capacity in all regions (GW)
+        :hydroinvestmentsallowed => false,
+        :transmissionallowed => :all,       # :none, :islands, :all
+        :hours => 1,                        # 1,2,3 or 6 hours per period
+        :solarwindarea => 1,                # area multiplier for GIS solar & wind potentials
+        :selectdays => 1,
+        :skipdays => 0,
+        :solver => :cplex,
+        :threads => 3,
+        :showsolverlog => true,
+        :rampingconstraints => false,
+        :rampingcosts => false,
+        :disabletechs => [],
+        :disableregions => [],
+        :islandindexes => [],               # [1:8, 9:15, 16:21] for eurasia21
+        :resultsfile => "results.jld2"      # use "" to skip saving the results in the database
+    )
 
 function autorunname(options)
-	name = ""
-	for (key,value) in setdiff(options, defaultoptions())
-		name *= "$key=$value, "
-	end
-	if isempty(name)
-		name = "default"
-	else
-		name = name[1:end-2]
-	end
-	name
+    name = ""
+    for (key,value) in setdiff(options, defaultoptions())
+        name *= "$key=$value, "
+    end
+    if isempty(name)
+        name = "default"
+    else
+        name = name[1:end-2]
+    end
+    name
 end
 
 function buildmodel(; optionlist...)
-	options, hourinfo, sets, params = buildsetsparams(; optionlist...)
-	return buildvarsmodel(options, hourinfo, sets, params)
+    options, hourinfo, sets, params = buildsetsparams(; optionlist...)
+    return buildvarsmodel(options, hourinfo, sets, params)
 end
 
 function buildsetsparams(; optionlist...)
-	println("\nReading input data...")
-	options = merge(defaultoptions(), optionlist)
-	hourinfo = HourSampling(options)
-	@time sets = makesets(hourinfo, options)
-	@time params = makeparameters(sets, options, hourinfo)
-	return options, hourinfo, sets, params
+    println("\nReading input data...")
+    options = merge(defaultoptions(), optionlist)
+    hourinfo = HourSampling(options)
+    @time sets = makesets(hourinfo, options)
+    @time params = makeparameters(sets, options, hourinfo)
+    return options, hourinfo, sets, params
 end
 
 function buildvarsmodel(options, hourinfo, sets, params)
-	println("\nBuilding model...")
-	modelname = initjumpmodel(options)
-	print("  - variables:   ")
-	@time vars = makevariables(modelname, sets)
-	print("  - extra bounds:")
-	@time setbounds(sets, params, vars, options)
-	print("  - constraints: ")
-	@time constraints = makeconstraints(modelname, sets, params, vars, hourinfo, options)
-	print("  - objective:   ")
-	@time makeobjective(modelname, sets, vars)
+    println("\nBuilding model...")
+    modelname = initjumpmodel(options)
+    print("  - variables:   ")
+    @time vars = makevariables(modelname, sets)
+    print("  - extra bounds:")
+    @time setbounds(sets, params, vars, options)
+    print("  - constraints: ")
+    @time constraints = makeconstraints(modelname, sets, params, vars, hourinfo, options)
+    print("  - objective:   ")
+    @time makeobjective(modelname, sets, vars)
 
-	return ModelInfo(modelname, sets, params, vars, constraints, hourinfo, options)	
+    return ModelInfo(modelname, sets, params, vars, constraints, hourinfo, options) 
 end
 
 # BASIC USAGE: (carbon tax 50 €/ton CO2, 3-hour time periods)
 # m, annualelec, capac, tcapac, chart = runmodel(carboncap=50, hours=3, [more options]...);
-function runmodel(; name="", group="", optionlist...)		# carbon tax in €/ton CO2
-	model = buildmodel(; optionlist...)
+function runmodel(; name="", group="", optionlist...)       # carbon tax in €/ton CO2
+    model = buildmodel(; optionlist...)
 
-	println("\nSolving model...")
-	#writeMPS(model, "model3.mps")
+    println("\nSolving model...")
+    #writeMPS(model, "model3.mps")
 
-	status = solve(model.modelname)
-	println("\nSolve status: $status")
+    status = solve(model.modelname)
+    println("\nSolve status: $status")
 
-	println("\nReading results...")
-	results = readresults(model, status)
+    println("\nReading results...")
+    results = readresults(model, status)
 
-	filename = model.options[:resultsfile]
+    filename = model.options[:resultsfile]
 
-	if !isempty(filename)
-		if isempty(name)
-			name = autorunname(model.options)
-		end
-		println("\nSaving results to disk...")
-		saveresults(results, name, resultsfile=filename, group=group)
-	end
+    if !isempty(filename)
+        if isempty(name)
+            name = autorunname(model.options)
+        end
+        println("\nSaving results to disk...")
+        saveresults(results, name, resultsfile=filename, group=group)
+    end
 
-	if status == :Optimal
-		annualelec, capac, tcapac, chart = analyzeresults(results)
-	else
-		annualelec, capac, tcapac, chart = nothing, nothing, nothing, nothing
-	end
+    if status == :Optimal
+        annualelec, capac, tcapac, chart = analyzeresults(results)
+    else
+        annualelec, capac, tcapac, chart = nothing, nothing, nothing, nothing
+    end
 
-	return results, annualelec, capac, tcapac, chart
+    return results, annualelec, capac, tcapac, chart
 end
 
 end #module
